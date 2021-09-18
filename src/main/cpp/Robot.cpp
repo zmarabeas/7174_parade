2613069f5466f76ab0ba092179d4103bdca11195
Zach Marabeas <zmarabeas@gmail.com>
2021-09-18 11:35:02 -0400

    simple controls for a parade

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -3,7 +3,6 @@
 // the WPILib BSD license file in the root directory of this project.
 
 #include "Robot.h"
-#include "SwerveDrive.hpp"
 #include <iostream>
 #include <frc/smartdashboard/SmartDashboard.h>
 #include <frc/shuffleboard/Shuffleboard.h>
@@ -13,176 +12,50 @@
 
 using namespace frc;
 
-void Robot::RobotInit() {
-    m_chooser.SetDefaultOption(kAutoNameDefault, kAutoNameDefault);
-    m_chooser.AddOption(kAutoNameCustom, kAutoNameCustom);
-    frc::SmartDashboard::PutData("Auto Modes", &m_chooser);
-    // gyro.SetYaw(0);
-    // a_gyro.Calibrate();
+void Robot::RobotInit() { }
+void Robot::RobotPeriodic() { }
+void Robot::AutonomousInit() { }
+void Robot::AutonomousPeriodic() { }
+void Robot::TeleopInit() { }
 
-
-}
-void Robot::RobotPeriodic() {
-
-//     double xvv = driveController.GetX(GenericHID::JoystickHand::kLeftHand);
-  //     double yvv = driveController.GetY(GenericHID::JoystickHand::kLeftHand);
-
-          frc::SmartDashboard::PutNumber("Shooter Angle", shooterAngle);
-
-          frc::SmartDashboard::PutNumber("Get Shooter Angle angle left", shooterAngleServoLeft.GetAngle());
-          frc::SmartDashboard::PutNumber("Get Shooter Angle angle Right", shooterAngleServoRight.GetAngle());
-
-          frc::SmartDashboard::PutNumber("Get Shooter angle speed left", shooterAngleServoLeft.GetSpeed());
-          frc::SmartDashboard::PutNumber("Get Shooter angle speed Right", shooterAngleServoRight.GetSpeed());   
-
-         frc::SmartDashboard::PutNumber("shooter motor velocity", shooterMotor.GetSelectedSensorVelocity());
-
-
-
-
-
-      frc::SmartDashboard::PutNumber("drive controller x", driveController.GetX(GenericHID::JoystickHand::kLeftHand));
-      frc::SmartDashboard::PutNumber("drive controller y", driveController.GetY(GenericHID::JoystickHand::kLeftHand));
-      frc::SmartDashboard::PutNumber("front Left encoder", frontLeftAngleMotor.GetSelectedSensorPosition());
-      frc::SmartDashboard::PutNumber("front Right encoder", frontRightAngleMotor.GetSelectedSensorPosition());
-       frc::SmartDashboard::PutNumber("front Left encoder drive", frontLeftDriveMotor.GetSelectedSensorPosition());
-      frc::SmartDashboard::PutNumber("front Right encoder drive", frontRightDriveMotor.GetSelectedSensorPosition());
-    
-
-  //     driveSpeedd = sqrt((xvv) * (xvv) + (yvv) * (yvv));  
-    
-  //     if (driveController.GetX(GenericHID::JoystickHand::kLeftHand) < 0){
-  //           driveAngle = (driveController.GetX(GenericHID::JoystickHand::kLeftHand)) * 90;
-
-  //     } else {
-  //           driveAngle = (-driveController.GetX(GenericHID::JoystickHand::kLeftHand)) * 90;
-
-  //     }
-    //  sensorPos = frontRightDriveMotor.GetSelectedSensorPosition();
- }
-
-void Robot::AutonomousInit() {
-    frontRightAngleMotor.SetSelectedSensorPosition(0);
-    frontLeftAngleMotor.SetSelectedSensorPosition(0);
-    backRightAngleMotor.SetSelectedSensorPosition(0);
-    backLeftAngleMotor.SetSelectedSensorPosition(0);
-
-    frontRightDriveMotor.SetSelectedSensorPosition(0);
-    frontLeftDriveMotor.SetSelectedSensorPosition(0);
-    backRightDriveMotor.SetSelectedSensorPosition(0);
-    backLeftDriveMotor.SetSelectedSensorPosition(0);
-        shooterMotor.Set(ControlMode::PercentOutput,0);
-    //gyro.SetYaw(0);
-}
-
-void Robot::AutonomousPeriodic() {
-if (frontLeftDriveMotor.GetSelectedSensorPosition() < 50000){ //drive straight
-    inputX = 0;
-    inputY = -0.3;
-     }
-else if (frontLeftDriveMotor.GetSelectedSensorPosition() < 110000){ //drive left
-    inputX = -0.3;
-    inputY = 0;
-    }
-else if (frontLeftDriveMotor.GetSelectedSensorPosition() < 275000){ //drive str
-    inputX = 0;
-    inputY = -0.3;
-    }
-else if (frontLeftDriveMotor.GetSelectedSensorPosition() < 330000){ //drive right
-    inputX = 0.3;
-    inputY = 0;
-    }
-else if (frontLeftDriveMotor.GetSelectedSensorPosition() < 390000){ //drive stright
-    inputX = 0;
-    inputY = -0.3;
-    }
-else if (frontLeftDriveMotor.GetSelectedSensorPosition() < 450000){ //drive 90 left
-    inputX = -0.3;
-    inputY = 0;
-    }
-else if (frontLeftDriveMotor.GetSelectedSensorPosition() < 505000){ //drive back
-    inputX = 0;
-    inputY = 0.3;
-    }
-else if (frontLeftDriveMotor.GetSelectedSensorPosition() < 565000){ //drive right
-    inputX = 0.3;
-    inputY = 0;
-    }
-// else if (frontLeftDriveMotor.GetSelectedSensorPosition() < 800000){ //drive back
-//     inputX = 0;
-//     inputY = 1;
-//     }
-/////////////////////////
-// else if (frontLeftDriveMotor.GetSelectedSensorPosition() < 780000){ //drive left
-//     inputX = -1;
-//     inputY = 0;
-//     }
-// else if (frontLeftDriveMotor.GetSelectedSensorPosition() < 840000){ //drive back
-//     inputX = 0;
-//     inputY = 1;
-//     }
-else { //STOP
-inputY = 0;
-inputX = 0;
-}
-driveTrain.drive(inputX, inputY, inputOmega, 1);
- }
-
-void Robot::TeleopInit() {
-        //gyro.SetYaw(180);
-        //a_gyro.Calibrate();
-        //a_gyro.Reset();
-table->PutNumber("ledMode",3);
-table->PutNumber("camMode",0);
-
-    frontRightAngleMotor.SetSelectedSensorPosition(0);
-    frontLeftAngleMotor.SetSelectedSensorPosition(0);
-    backRightAngleMotor.SetSelectedSensorPosition(0);
-    backLeftAngleMotor.SetSelectedSensorPosition(0);
-
-    frontLeftDriveMotor.SetSelectedSensorPosition(0);
-    frontRightDriveMotor.SetSelectedSensorPosition(0);
-    backLeftDriveMotor.SetSelectedSensorPosition(0);
-    backRightDriveMotor.SetSelectedSensorPosition(0);
-
-
-}
 void Robot::TeleopPeriodic() {
+    //drive
+    drivetrain.ArcadeDrive(driveController.GetY(GenericHID::JoystickHand::kLeftHand), 
+        driveController.GetX(GenericHID::JoystickHand::kRightHand))
+    
+    //INTAKE CONTROL.
+    // if(controlController.GetYButton()){
+    //         intakeMotor.Set(ControlMode::PercentOutput,-1);
+    // }else if (controlController.GetXButton())	{
+    //     intakeMotor.Set(ControlMode::PercentOutput,0);
+    // }
 
-// frc::SmartDashboard::PutNumber("gyro angle", a_gyro.GetAngle());
-// frc::SmartDashboard::PutNumber("gyro offset", a_gyro.GetOffset());
-
-      if (driveController.GetTriggerAxis(GenericHID::JoystickHand::kLeftHand) > .1){
-        table->PutNumber("ledMode",1);
-                speedMul = 0;
-        std::cout<<"pressed"<<std::endl;     
-   table->PutNumber("camMode",1);
-
-    }else{
-        table->PutNumber("ledMode",3);
-        table->PutNumber("camMode",0);
-speedMul = table->GetNumber("tx",0.0);
-
-        
-
-}
- driveTrain.drive(driveController.GetX(GenericHID::JoystickHand::kLeftHand), driveController.GetY(GenericHID::JoystickHand::kLeftHand), (driveController.GetX(GenericHID::JoystickHand::kRightHand)*0.6), speedMul);
-//  driveTrain.drive(LogitechStick.GetX(),LogitechStick.GetY(),(LogitechStick.GetTwist()*0.5),speedMul);   
-//FEEDER CONTROL.
-
-    // if (driveController.GetAButton()){
-    //   feedPeriodic();
+    // if(controlController.GetAButton()){
+    //     intakeLiftMotor.Set(ControlMode::PercentOutput,0.2);
+    // }else if (controlController.GetBButton()){
+    //     intakeLiftMotor.Set(ControlMode::PercentOutput,-0.2);
     // }else{
-    //   feedON = false;
-    //   feedElpasedTime = 0;
-    //   prevFeedTime = time.Get();
-    //   feederMotor.Set(ControlMode::PercentOutput,0.0);
-    //  }
+    //     intakeLiftMotor.Set(ControlMode::PercentOutput,0);
+    // }
 
-    if(driveController.GetBackButtonReleased()){
-        //gyro.SetYaw(180);
+
+    manualAngleControl();
+    //Angle Motor
+    if (controlController.GetBButton()) {
+        shooterAngle = 0.456;
+        shooterValue = 0.725;
+    }else if (controlController.GetAButton()){
+        shooterAngle = 0.3600;
+        shooterValue = 1;
+    }else if (controlController.GetXButton()){
+        shooterAngle = 0.33300;//.342
+        shooterValue = 1;
+    }else{
+        shooterAngleServoLeft.Set(shooterAngle);
+        shooterAngleServoRight.Set(shooterAngle);
     }
 
+    // feeder
     if (driveController.GetTriggerAxis(GenericHID::JoystickHand::kLeftHand) > .1 && shooterMotor.GetSelectedSensorVelocity() > 16500){
         feederMotor.Set(ControlMode::PercentOutput,0.5);
 
@@ -191,96 +64,23 @@ speedMul = table->GetNumber("tx",0.0);
 
     } else {
         feederMotor.Set(ControlMode::PercentOutput,0);
-
-
     }
-    if (driveController.GetYButton())
-    {
+
+    if (driveController.GetYButton()){
         double feederEncoder = feederMotor.GetSelectedSensorPosition() + 2048;
-        while (feederMotor.GetSelectedSensorPosition() < feederEncoder)
-        {        feederMotor.Set(ControlMode::PercentOutput,0.5);
-}
+        while (feederMotor.GetSelectedSensorPosition() < feederEncoder){
+            feederMotor.Set(ControlMode::PercentOutput,0.5);
+        }
         feederMotor.Set(ControlMode::PercentOutput,0);
-
-        
     }
-    
-//
-
- //INTAKE CONTROL.
-        if(controlController.GetYButton()){
-                intakeMotor.Set(ControlMode::PercentOutput,-1);
-
-        } else if (controlController.GetXButton())	{
-            intakeMotor.Set(ControlMode::PercentOutput,0);
-
-        }
-
-        if(controlController.GetAButton()){
-            intakeLiftMotor.Set(ControlMode::PercentOutput,0.2);
-
-        }else if (controlController.GetBButton()){
-            intakeLiftMotor.Set(ControlMode::PercentOutput,-0.2);
-
-        }else{
-            intakeLiftMotor.Set(ControlMode::PercentOutput,0);
-
-        }
-        //Angle Motor
-        if (controlController.GetBButton()) {
-            shooterAngle = 0.456;
-            shooterValue = 0.725;
-        }else if (controlController.GetAButton()){
-            shooterAngle = 0.3600;
-            shooterValue = 1;
-        }else if (controlController.GetXButton()){
-            shooterAngle = 0.33300;//.342
-            shooterValue = 1;
-    }else{
-        shooterAngleServoLeft.Set(shooterAngle);
-        shooterAngleServoRight.Set(shooterAngle);
-    }
-    
-        
-        
-//
-
-
-//SHOOTER MOTOR CONTROL.
- 
-     manualAngleControl();
-
+    // shooter
     if (driveController.GetTriggerAxis(GenericHID::JoystickHand::kRightHand) > .1 ){
-        // shooterMotor.Set(ControlMode::PercentOutput,driveController.GetTriggerAxis(GenericHID::JoystickHand::kRightHand));
-                shooterMotor.Set(ControlMode::PercentOutput,.9);
-
-        // shooterMotor.Set(shooterValue);
-
-        // std::cout<<"SHOOTER MOTOR ON"<<std::endl;
-
+        shooterMotor.Set(ControlMode::PercentOutput,.9);
     } else if (driveController.GetBumper(GenericHID::JoystickHand::kRightHand) == true){
         shooterMotor.Set(ControlMode::PercentOutput,-.5);
-        // std::cout<<"SHOOTER MOTOR REVERSE"<<std::endl;
-
     } else{
         shooterMotor.Set(ControlMode::PercentOutput, 0);
-        // std::cout<<"SHOOTER MOTOR OFF"<<std::endl;
-
     }
-//
-
-
-
-    // if (controlController.GetY(GenericHID::JoystickHand::kLeftHand) < -0.1){
-    //     shooterAngle = shooterAngle + 0.003;
-    // }else if(controlController.GetY(GenericHID::JoystickHand::kLeftHand) > 0.1){
-    //     shooterAngle = shooterAngle - 0.003;
-    // } 
-    // shooterAngle = std::clamp(shooterAngle,-1.0, 1.0);
-    // shooterAngleServoLeft.SetSpeed(shooterAngle);
-    // shooterAngleServoRight.SetSpeed(shooterAngle);
-    // std::cout << shooterAngle << std::endl;
-
 }
 
 void Robot::DisabledInit() {}
@@ -290,12 +90,6 @@ void Robot::TestInit() {}
 void Robot::TestPeriodic() {}
 
 void Robot::manualAngleControl(){
-    // if (arcade.GetRawButton(12)){
-    //     shooterAngle = shooterAngle + 0.001;
-
-    // }else if(arcade.GetRawButton(11)){
-    //     shooterAngle = shooterAngle - 0.001;
-    // } 
     if (controlController.GetY(GenericHID::JoystickHand::kLeftHand) < -0.1){
         shooterAngle = shooterAngle + 0.003;
     }else if(controlController.GetY(GenericHID::JoystickHand::kLeftHand) > 0.1){
@@ -305,6 +99,8 @@ void Robot::manualAngleControl(){
     shooterAngleServoLeft.Set(shooterAngle);
     shooterAngleServoRight.Set(shooterAngle);
 }
+
+
 #ifndef RUNNING_FRC_TESTS
 int main() {
   return frc::StartRobot<Robot>();
--- src/main/include/Robot.h
+++ src/main/include/Robot.h
@@ -16,204 +16,103 @@
 #include <frc/Joystick.h>
 #include <frc/PowerDistributionPanel.h>
 #include <frc/I2C.h>
-//pathweaver
-#include <frc/Filesystem.h>
-#include <frc/trajectory/TrajectoryUtil.h>
-#include <wpi/Path.h>
-#include <wpi/SmallString.h>
-#include "SwerveDrive.hpp"
-
 
 std::shared_ptr<NetworkTable> table = nt::NetworkTableInstance::GetDefault().GetTable("limelight");
 
-//
-
 using namespace frc;
 
+
+#define FRONT_LEFT_MOTOR 3
+#define BACK_LEFT_MOTOR 6
+#define FRONT_RIGHT_MOTOR 1
+#define BACK_RIGHT_MOTOR 2
+
+#define FEEDER_MOTOR 9
+#define SHOOTER_MOTOR 15
+
+#define ANGLE_SERVO_LEFT 5
+#define ANGLE_SERVO_RIGHT 6
+
 class Robot : public frc::TimedRobot {
- public:
-  void RobotInit() override;
-  void RobotPeriodic() override;
+    public:
+        void RobotInit() override;
+        void RobotPeriodic() override;
 
-  void AutonomousInit() override;
-  void AutonomousPeriodic() override;
+        void AutonomousInit() override;
+        void AutonomousPeriodic() override;
 
-  void TeleopInit() override;
-  void TeleopPeriodic() override;
+        void TeleopInit() override;
+        void TeleopPeriodic() override;
 
-  void DisabledInit() override;
-  void DisabledPeriodic() override;
+        void DisabledInit() override;
+        void DisabledPeriodic() override;
 
-  void TestInit() override;
-  void TestPeriodic() override;
+        void TestInit() override;
+        void TestPeriodic() override;
 
-  void manualAngleControl();
-
-
-  private:
-   frc::SendableChooser<std::string> m_chooser;
-    const std::string kAutoNameDefault = "Default";
-    const std::string kAutoNameCustom = "My Auto";
-    std::string m_autoSelected;
-
-    PowerDistributionPanel PDP{0};
-
-//CONTROLLER VALUES
-    XboxController driveController{0};
-    XboxController controlController{1};
-    Joystick LogitechStick{2};
-    
-    double speedMul = 0;
-
-    //Joystick arcade {2};
-
-    bool driveButtonA = driveController.GetAButton();
-    bool driveButtonB = driveController.GetBButton();
-    bool driveButtonY = driveController.GetYButton();
-    bool driveButtonX = driveController.GetXButton();
-
-    bool controlButtonA = controlController.GetAButton();
-    bool controlButtonB = controlController.GetBButton();
-    bool controlButtonY = controlController.GetYButton();
-    bool controlButtonX = controlController.GetXButton();
-
-// 
+        void manualAngleControl();
 
 
-#ifdef PRACTICE
-    
-    //practice.
-    WPI_TalonSRX shooterMotor{2}; 
-    WPI_TalonSRX feederMotor {0}; 
-    WPI_TalonSRX climberMotor{0};  
-    WPI_TalonSRX elevatorMotor{0};
-    WPI_TalonSRX intakeMotor{0};
-    WPI_TalonSRX wheelOfFortuneMotor{0};
-    Servo shooterAngleServo {9};
+    private:
+        frc::SendableChooser<std::string> m_chooser;
+        const std::string kAutoNameDefault = "Default";
+        const std::string kAutoNameCustom = "My Auto";
+        std::string m_autoSelected;
+        PowerDistributionPanel PDP{0};
+
+        XboxController driveController{0};
+        XboxController controlController{1};
+        Joystick LogitechStick{2};
+        
+        double speedMul = 0;
 
 
-    WPI_TalonSRX frontLeftMotor{4}; 
-    WPI_TalonSRX backLeftMotor{3};  
-    SpeedControllerGroup leftSideMotors{frontLeftMotor, backLeftMotor};
+        // drivetrain
+        WPI_TalonSRX frontLeftMotor{FRONT_LEFT_MOTOR}; 
+        WPI_TalonSRX backLeftMotor{BACK_LEFT_MOTOR};  
+        SpeedControllerGroup leftSideMotors{frontLeftMotor, backLeftMotor};
+        WPI_TalonSRX frontRightMotor{FRONT_RIGHT_MOTOR};  
+        WPI_TalonSRX backRightMotor{BACK_RIGHT_MOTOR}; 
+        SpeedControllerGroup rightSideMotors{frontRightMotor, backRightMotor};
 
-    WPI_TalonSRX frontRightMotor{1};  
-    WPI_TalonSRX backRightMotor{2}; 
-    SpeedControllerGroup rightSideMotors{frontRightMotor, backRightMotor};
+        DifferentialDrive drivetrain{leftSideMotors, rightSideMotors};
 
-    DifferentialDrive drivetrain{leftSideMotors, rightSideMotors};
+        //ACTIONARY MOTORS
+        WPI_TalonSRX shooterMotor{SHOOTER_MOTOR}; 
+        WPI_TalonSRX feederMotor{FEEDER_MOTOR}; 
+        Servo shooterAngleServoLeft {ANGLE_SERVO_LEFT};
+        Servo shooterAngleServoRight {ANGLE_SERVO_RIGHT};
 
+        /* not in use */
+        // WPI_TalonSRX intakeMotor{8};//9
+        // WPI_TalonSRX intakeLiftMotor{100};//10
+        // WPI_TalonSRX wheelOfFortuneMotor {11};
 
-    
-//
-#else//main robot
-//DRIVETRAIN
-        WPI_TalonSRX frontLeftDriveMotor = 20;
-		WPI_TalonSRX frontLeftAngleMotor = 21;
+        //DRIVETRAIN STATES
+        bool aunkrModeState = false;
+        bool rawModeState = false;
+        bool climbModeState = false;
+        bool ballTrackState = false;
+        double driveSpeed = 0;
+        double turnSpeed = 0;
 
-		WPI_TalonSRX frontRightDriveMotor = 10;
-		WPI_TalonSRX frontRightAngleMotor = 11;
+        Timer time;
+        double buttonPressedTime = 0;
+        double prevFeedTime = 0.0;
+        double feedOffTime = 1.0;
+        double feedOnTime = 1.0;
+        double feedElpasedTime = 0.0;
+        bool feedON = false;
+        
+        uint8_t buffer = 0;
+        I2C arduino {I2C::Port::kOnboard, 0x03};
 
-		WPI_TalonSRX backLeftDriveMotor = 30;
-		WPI_TalonSRX backLeftAngleMotor = 31;
+        double shooterAngle = 0.345000;//manual control of servo//.7 angle servo init line inner port
+        double shooterAngleTrench = -0.67;//.68 angle servo trench
 
-		WPI_TalonSRX backRightDriveMotor = 40;
-		WPI_TalonSRX backRightAngleMotor = 41;
+        double sensorPos = 0;
+        double driveSpeedd = 0;
+        double driveAngle = 0;
 
-//     WPI_TalonSRX frontLeftMotor{1}; 
-//     WPI_TalonSRX backLeftMotor{2};  
-//     SpeedControllerGroup leftSideMotors{frontLeftMotor, backLeftMotor};
-    
-//     WPI_TalonSRX frontRightMotor{3};  
-//     WPI_TalonSRX backRightMotor{4}; 
-//     SpeedControllerGroup rightSideMotors{frontRightMotor, backRightMotor};
-
-//     DifferentialDrive drivetrain{leftSideMotors, rightSideMotors};
-// //
-//ACTIONARY MOTORS
-    WPI_TalonSRX shooterMotor{15}; 
-    WPI_TalonSRX feederMotor{9}; 
-    WPI_TalonSRX intakeMotor{8};//9
-    WPI_TalonSRX intakeLiftMotor{100};//10
-    WPI_TalonSRX wheelOfFortuneMotor {11};
-    Servo shooterAngleServoLeft {5};
-    Servo shooterAngleServoRight {6};
-
-//
-#endif
-
-//DRIVETRAIN STATES
-    bool aunkrModeState = false;
-    bool rawModeState = false;
-    bool climbModeState = false;
-    bool ballTrackState = false;
-    double driveSpeed = 0;
-    double turnSpeed = 0;
-//
-
-    Timer time;
-    double buttonPressedTime = 0;
-    double prevFeedTime = 0.0;
-    double feedOffTime = 1.0;
-    double feedOnTime = 1.0;
-    double feedElpasedTime = 0.0;
-    bool feedON = false;
-    
-    uint8_t buffer = 0;
-    I2C arduino {I2C::Port::kOnboard, 0x03};
-
-//Best shooter Values
-    double centerPortHeight = 8.225;
-    double cameraHeight = 40.0 / 12.0;
-    double cameraAngleRAD = 17 * M_PI / 180.0;
-    double tYTargetDEG = 0;
-    double distanceFromWallFT = 0;
-    double cameraToBumper = 15.5 / 12.0;
-    
-    // std::array<double, 6> targetDistanceFtArray = {0,1,2,3,4,5};
-    // std::array<double, 6> shooterAngleArray = {-1,0,0,0,0,1};
-    double targetDistanceFtArray[6] = {6,5,4,3,2,1};
-    double shooterAngleArray[6] = {-1,0,0,0,0,1};
-
-    double shooterAngle = 0.345000;//manual control of servo//.7 angle servo init line inner port
-    double shooterAngleTrench = -0.67;//.68 angle servo trench
-//
-
-//Auton Values.
-    int autonState = 1;
-    double encoderCPR = 2048.0 * 10.71; //one wheel rotation
-    double wheelDiameterFt = 0.5; //6 inches
-    double rightDistanceTraveledFt = 0.0;
-    double leftDistanceTraveledFt = 0.0;
-    double goalDistance = 3.0; 
-    double leftEncoderCount = 0;
-    double rightEncoderCount = 0;
-    // double prevLeftEncoderCount = frontLeftMotor.GetSelectedSensorPosition();
-    // double prevRightEncoderCount = frontRightMotor.GetSelectedSensorPosition();
-    double prevRightX = 0;
-    double prevLeftY = 0;
-
-    double goalTargetPosition = 0;
-//
-    double Tf = 0.30;
-    double Ts = 1.0/50.0;
-    double a = Ts/(Tf + Ts);
-
-    double driveSpeedd = 0;
-    double driveAngle = 0;
-
-
-    double sensorPos = 0;
-   
-		PigeonIMU gyro = (0);
-        //AnalogGyro a_gyro{1};
-
-    double inputX = 0;
-    double inputY = 0;
-    double inputOmega = 0;
-
-    double shooterValue = 0;
-    
-
-    SwerveDrive driveTrain;
-
+        double shooterValue = 0;
 };
